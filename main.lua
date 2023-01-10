0cd95a07c998e2e1c1c0203721720a454e5a068c
umoko <122369042+umoko@users.noreply.github.com>
2023-01-10 19:02:25 +0100

    Update main.lua
    catching event and promo msg

--- main.lua
+++ main.lua
@@ -1,1 +1,10 @@
-C_NamePlate.SetNamePlateFriendlySize(60, 30)
+local myFrame = CreateFrame("Frame")
+myFrame:HookScript("OnEvent",
+  function()
+    C_NamePlate.SetNamePlateFriendlySize(60, 30)
+  end
+);
+myFrame:RegisterEvent("PLAYER_LOGIN")
+
+
+print('smallFriendlyNameplate Loaded, support me on http://twitch.tv/umoko !')
